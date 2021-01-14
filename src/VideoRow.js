ee586db012a499851c2fe86ae9c5827632add40d
Gozel Cholukova <gozelr@mail.ru>
2021-01-14 22:25:50 +0500

    Add files via upload

--- src/VideoRow.js
+++ src/VideoRow.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import "./VideoRow.css";
 
-function VideoRow() {
+function VideoRow({ views, subs, description, timestamp, channel, title, image }) {
   return ( 
     <div className="videoRow">
-      
+      <img src={image} alt=""/>
+
+      <div className="videoRow__text">
+        <h3>{title}</h3>
+        <p className="videoRow__headline"> {channel} • {subs} Subscribers {views} views • {timestamp}</p>
+        <p>{description}</p>
+      </div>
     </div>
   );
 }
